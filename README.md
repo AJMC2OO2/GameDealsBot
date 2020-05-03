a8c2e9d838d90b1fb3a719f949514fd0c9faceb0
Adrián Mosquera C <64504839+AJMC2OO2@users.noreply.github.com>
2020-05-03 16:37:51 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# GratisBot
-GratisBot mío, usando el repositorio de uro98
+# Gratis
+
+This is a Discord bot that scrapes the [r/GameDeals](https://www.reddit.com/r/GameDeals/) subreddit for free deals. It sends new deals to channels called "free-games" at 6am and 6pm every day.
+
+## Built With
+
+* [PRAW](https://github.com/praw-dev/praw) - Python Reddit API Wrapper
+* [discord.py](https://github.com/Rapptz/discord.py) - Python Discord API Wrapper
+
+## Author
+
+Yu-Jo Tseng
+
+## License
+
+This project is licensed under the BSD 2-Clause License - see the [LICENSE](LICENSE) file for details.
---
+++ reddit_scraper.py
@@ -0,0 +1,18 @@
+import config
+import praw
+
+
+class RedditScraper:
+
+    def __init__(self, config=config):
+        self.reddit = praw.Reddit(client_id="PWzL26MwPoKw_g",
+                                  client_secret="9hy-8izjQxrJQErjcQ4-RwkDKlU",
+                                  password="<REDDIT_PASSWORD>",
+                                  user_agent="Gratisv1",
+                                  username="<REDDIT_USERNAME>")
+        self.reddit.read_only = True
+
+    def get_new_posts_in_sub(self, num_of_posts, sub_name):
+        """Get the specified number of new posts from the subreddit"""
+        sub = self.reddit.subreddit("GameDeals")
+        return sub.new(limit=num_of_posts)
