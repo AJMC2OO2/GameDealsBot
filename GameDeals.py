ec151580d3fc8aec62459b3abe003c818a18d27b
Adrián Mosquera C <64504839+AJMC2OO2@users.noreply.github.com>
2020-05-05 21:45:34 -0500

    Add files via upload

--- GameDeals.py
+++ GameDeals.py
@@ -24,11 +24,11 @@ class GratisClient(discord.Client):
     async def on_message(self, message):
         if message.content in ['gd.info', 'gd.i']:
             description = 'Game Deals is a bot that helps you know the latest discounts on videogames you\'ll keep forever in your library!'
-            embed = discord.Embed(title='Bot Information', description=description, colour=0x2df228) \
+            embed = discord.Embed(title='Bot Information', description=description, colour=0xa865e3) \
                 .add_field(name='Developer', value='[<REDDIT_USERNAME>](https://github.com/AJMC2OO2)')
             await message.channel.send(embed=embed)
         elif message.content in ['gd.help', 'gd.h']:
-            embed = discord.Embed(title='Game Deals Commands', colour=0x2df228) \
+            embed = discord.Embed(title='Game Deals Commands', colour=0xa865e3) \
                 .add_field(name='gd.info', value='Display bot information.', inline=False) \
                 .add_field(name='gd.help', value='Display the commands menu.')
             await message.channel.send(embed=embed)
@@ -45,15 +45,15 @@ class GratisClient(discord.Client):
         channel = self.get_channel(config.DISCORD_CHANNEL_ID)
 
         while not self.is_closed():
-            if self.__between_6am_and_6pm():
+            if self.__between_12am_and_12pm():
                 new_free_deals = manager.find_deals()
 
                 if new_free_deals:
                     for deal in new_free_deals:
                         embed = discord.Embed(
-                            title=deal.title[deal.title.find(" ")+1:len(deal.title)],
+                            title=deal.title[deal.title.find(" ")+1:len(x)],
                             description=deal.title[1:deal.title.find(" ") - 1],
-                            url=deal.url,
+                            url=deal.url
                             color=0x2df228
                         )
                         await self.__send_deals(embed)
@@ -63,12 +63,12 @@ class GratisClient(discord.Client):
             else:
                 await asyncio.sleep(1)
 
-    def __between_6am_and_6pm(self):
+    def __between_12am_and_12pm(self):
         """
         Return true if current time is within 6am or 6pm.
         """
         current_time = datetime.now()
-        return ((current_time.hour >= 6) or (current_time.hour <= 18)) and (current_time.minute == 0)
+        return ((current_time.hour >= 12) and (current_time.hour <= 24)) and (current_time.minute == 0)
 
     async def __send_deals(self, embed):
         channels_to_send_to = [c for c in self.get_all_channels(
