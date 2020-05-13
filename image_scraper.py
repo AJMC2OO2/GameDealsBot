7fe4add97089a393ba9b4e62c92a7b19d74ab173
Adrián Mosquera C <64504839+AJMC2OO2@users.noreply.github.com>
2020-05-13 16:07:23 -0500

    Update image_scraper.py

--- image_scraper.py
+++ image_scraper.py
@@ -6,13 +6,7 @@ from selenium import webdriver
 
 class ImagePreview:
     def __init__(self):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get(
-            "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        pass
 
     def ogimage(url, code):
         og_image = html.find_all(property="og:image")
@@ -27,6 +21,14 @@ class ImagePreview:
             break
 
     def preview_image(self,url):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get(
+            "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        
         source = driver.get(url)
         html = driver.page_source
         code = BeautifulSoup(source.text, "html.parser")
